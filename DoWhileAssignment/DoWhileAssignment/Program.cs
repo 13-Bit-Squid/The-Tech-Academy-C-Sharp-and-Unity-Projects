de050932bf91700ac0c6df3b1d8ce619cc0f9ebc
13-Bit-Squid <pendragreen@gmail.com>
2021-06-09 15:43:37 -0600

    dowhileassignment

---
+++ DoWhileAssignment/DoWhileAssignment/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite number!");
+            int guessNum = Convert.ToInt32(Console.ReadLine());
+            bool favNum = guessNum == 13;
+
+            //while (!favNum)
+            //{
+            //    switch (guessNum)
+            //    {
+            //        case 12:
+            //            Console.WriteLine("Sorry, that's not the right number... You're so close! Try again!");
+            //            Console.WriteLine("Guess my favorite number!");
+            //            guessNum = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //        case 13:
+            //            Console.WriteLine("Congratulations!! You've guessed my favorite number! Pat yourself on the back, well done!");
+            //            favNum = true;
+            //            break;
+
+            //        case 14:
+            //            Console.WriteLine("Sorry, that's not the right number... You're so close! Try again!");
+            //            Console.WriteLine("Guess my favorite number!");
+            //            guessNum = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Sorry, that's not the right number... Try again!");
+            //            Console.WriteLine("Guess my favorite number!");
+            //            guessNum = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+            //Console.ReadLine();
+
+            do
+            {
+                switch (guessNum)
+                {
+                    case 12:
+                        Console.WriteLine("Sorry, that's not the right number... You're so close! Try again!");
+                        Console.WriteLine("Guess my favorite number!");
+                        guessNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 13:
+                        Console.WriteLine("Congratulations!! You've guessed my favorite number! Pat yourself on the back, well done!");
+                        favNum = true;
+                        break;
+
+                    case 14:
+                        Console.WriteLine("Sorry, that's not the right number... You're so close! Try again!");
+                        Console.WriteLine("Guess my favorite number!");
+                        guessNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry, that's not the right number... Try again!");
+                        Console.WriteLine("Guess my favorite number!");
+                        guessNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!favNum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int army = 4;
+            switch (army)
+            {
+                case 1:
+                    Console.WriteLine("Blood Angels");
+                    break;
+                case 2:
+                    Console.WriteLine("Imperial Fists");
+                    break;
+                case 3:
+                    Console.WriteLine("Dark Angels");
+                    break;
+                case 4:
+                    Console.WriteLine("Ultra Marines");
+                    break;
+                case 5:
+                    Console.WriteLine("White Scars");
+                    break;
+                case 6:
+                    Console.WriteLine("Iron Hands");
+                    break;
+                case 7:
+                    Console.WriteLine("Space Wolves");
+                    break;
+                case 8:
+                    Console.WriteLine("Raven Guard");
+                    break;
+                case 9:
+                    Console.WriteLine("Salamanders");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
