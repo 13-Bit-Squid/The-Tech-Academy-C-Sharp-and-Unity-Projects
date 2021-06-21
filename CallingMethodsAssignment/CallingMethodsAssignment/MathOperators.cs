8fdd02e5bea0fdf8554ed287b59a01a5428c108e
13-Bit-Squid <pendragreen@gmail.com>
2021-06-21 13:26:46 -0600

    callmethod

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathOperators.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class MathOperators
+    {
+        public static int AddItem(int userNum)
+        {
+            int sum;
+            int numA = userNum;
+            sum =  8 + numA;
+            return sum;
+        }
+        public static int SubItem(int userNum)
+        {
+            int diff;
+            int numB = userNum;
+            diff = 8 - numB;
+            return diff;
+        }
+        public static int MultItem(int userNum)
+        {
+            int prod;
+            int numC = userNum;
+            prod = 8 * numC;
+            return prod;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number to be added, subtracted, and multiplied by 8.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum of 8 plus " + userNum + " is: " + MathOperators.AddItem(userNum));
+            Console.WriteLine("The difference of 8 minus " + userNum + " is: " + MathOperators.SubItem(userNum));
+            Console.WriteLine("The product of 8 times " + userNum + " is: " + MathOperators.MultItem(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}
