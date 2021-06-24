b98f8d19de272490604e2eeb45a1c2930ecd89d1
13-Bit-Squid <pendragreen@gmail.com>
2021-06-24 13:08:46 -0600

    mainmethod

---
+++ MainMethodAssignment/MainMethodAssignment/Operations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Operations
+    {
+        public int MainOperator(int userInt)
+        {
+            int prod;
+            int numA = userInt;
+            prod = numA * 1;
+            return prod;
+        }
+
+        public int MainOperator(decimal userInt)
+        {
+            int wholeNum;
+            decimal numB = userInt;
+            wholeNum = Convert.ToInt32(numB);
+            return wholeNum;
+        }
+
+        public int MainOperator(string userStr)
+        {
+            int stringNum;
+            string numC = userStr;
+            stringNum = int.Parse(numC);
+            return stringNum;
+        }
+    }
+}
