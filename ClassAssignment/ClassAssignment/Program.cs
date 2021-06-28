7a3eee8fc2a9bc82ad59f10829ea21cdca2ecd12
13-Bit-Squid <pendragreen@gmail.com>
2021-06-28 17:27:51 -0600

    classassignment

---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidAgain voidAgain = new VoidAgain();
+            Console.WriteLine("Please input a number to be divided by 2.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            voidAgain.DivTwo(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/VoidAgain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class VoidAgain
+    {
+        public void DivTwo(int a)
+        {
+            int Quotient = a / 2;
+            Console.WriteLine("Your number {0}, divided by 2 is: {1}", a, Quotient);
+        }
+        static string DivTwo(string b)
+        {
+            Console.WriteLine("You really just put words in a math request??");
+            return b;
+        }
+    }
+}
