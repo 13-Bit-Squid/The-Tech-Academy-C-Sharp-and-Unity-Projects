2e37ee4b433d2f103b8297962c07e4efc70594e7
13-Bit-Squid <pendragreen@gmail.com>
2021-09-15 20:59:50 -0600

    porttolaptop

---
+++ ParamatersAssignment/ParamatersAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamatersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            
+
+            Console.WriteLine(employee1);
+            Console.ReadLine();
+        }
+    }
+}
