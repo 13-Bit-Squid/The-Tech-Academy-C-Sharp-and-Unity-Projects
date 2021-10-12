011ee330ae700adf2234e30997f095d3c3805bfe
13-Bit-Squid <pendragreen@gmail.com>
2021-10-12 15:04:32 -0600

    parseenum

---
+++ parsingEnums/parsingEnums/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parsingEnums
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                var x = Console.ReadLine();
+
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), x);
+                Console.WriteLine("Today is " + x);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ parsingEnums/parsingEnums/ProgramBase.cs
@@ -0,0 +1,17 @@
+﻿namespace parsingEnums
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                var x = Console.ReadLine();
+
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), x);
+                Console.WriteLine("Today is " + x);
+                Console.ReadLine();
+            }
+    }
+}
