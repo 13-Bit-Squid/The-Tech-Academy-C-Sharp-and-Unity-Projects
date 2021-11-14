358617e259cf6537157db958c595fc2a3bfc32e0
13-Bit-Squid <pendragreen@gmail.com>
2021-11-14 12:54:43 -0700

    timeassignment

---
+++ DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please input a number.");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(x));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InputChallenge/InputChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input any number of choice!");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"D:\\Documents\\Logs\\log2.txt", number);
+            string read = File.ReadAllText(@"D:\\Documents\\Logs\\log2.txt");
+            Console.WriteLine(read);
+            Console.ReadLine();
+        }
+    }
+}
